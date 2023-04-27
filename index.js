b8954ea82e1dcbfbfaaa27e5fe347664e8bf74f7
Anna Marlena <annamarlena310@gmail.com>
2023-04-26 19:46:20 -0500

    Fixed the bug in the prompt

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
 const inquirer = require("inquirer");
 const fs = require("fs"); 
-const { Shape, Circle, Square, Triangle } = require("./shapes")
+// const { Shape, Circle, Square, Triangle } = require("./shapes")
 
-inquirer.prompt([
-  {
-    type: 'input',
-    message: 'Please enter up to three characters for your logo',
-    chars: 'chars',
-  },
-  {
-    type: 'input',
-    message: 'Please enter a text color using a keyword or a hexadecimal number',
-    name: 'textColor',
-  },
-  {
-    type: 'list',
-    message: 'Please choose a logo shape',
-    name: 'shape',
-    choices: ['circle', 'square', 'triange'],
-  },
-  {
-    type: 'input',
-    message: 'Please enter a shape color using a keyword or a hexadecimal number',
-    name: 'shapeColor',
-  },
-])
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'Please enter up to three characters for your logo',
+      name: 'chars',
+    },
+    {
+      type: 'input',
+      message: 'Please enter a text color using a keyword or a hexadecimal number',
+      name: 'textColor',
+    },
+    {
+      type: 'list',
+      message: 'Please choose a logo shape',
+      name: 'chosenShape',
+      choices: ['circle', 'square', 'triange'],
+    },
+    {
+      type: 'input',
+      message: 'Please enter a shape color using a keyword or a hexadecimal number',
+      name: 'shapeColor',
+    },
+  ])
 
-.then((data) => {
-  const filename = `new-logo.svg`
-  const svg = createLogo(data)
+  .then((response) => {
+    console.log(response)
+  })
 
-  fs.writeFile(filename, svg, (err) =>
-  err ? console.log(err) : console.log('Your logo has been successfully created!')
-  );
-})
 
--- shapes.js
+++ shapes.js
@@ -1,22 +1,22 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
+// const inquirer = require("inquirer");
+// const fs = require("fs");
 
-class Shape {
-  constructor
-}
+// class Shape {
+//   constructor
+// }
 
-class Circle extends Shape {
-  constructor
-}
+// class Circle extends Shape {
+//   constructor
+// }
 
-class Square extends Shape {
-  constructor
-}
+// class Square extends Shape {
+//   constructor
+// }
 
-class Triangle extends Shape {
-  constructor
-}
+// class Triangle extends Shape {
+//   constructor
+// }
 
-module.exports = {
-  Shape, Circle, Square, Triangle
-}
+// module.exports = {
+//   Shape, Circle, Square, Triangle
+// }
